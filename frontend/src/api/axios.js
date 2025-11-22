be857407b8f0515247f3802dbe3a8ff0a6eaa153
stevenrao <rao_cx@mulangcloud.com>
2025-11-22 19:01:09 +0800

    优化搜索

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // 创建axios实例
 const api = axios.create({
-  baseURL: 'http://localhost:3002/api',
+  baseURL: '/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
